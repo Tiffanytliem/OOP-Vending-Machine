87742c5fb174e491512aa3b2bef609bd49107c4e
Tiffany Liem <thuytliem@gmail.com>
2022-07-08 18:39:45 -0700

    Add Java Files

---
+++ Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("\t************************************************");
+        System.out.println("\t             WELCOME TO JAVA DRINKS!            ");
+        System.out.println("\t************************************************");
+
+        Item[][] items = new Item[][] {
+                { new Item("Pepsi", 1.99, 3),
+                        new Item("Fresca", 1.49, 3),
+                        new Item("Brisk", 2.49, 2) },
+                { new Item("Fanta", 1.99, 2),
+                        new Item("Barq's", 1.49, 2),
+                        new Item("A & W", 2.49, 3) },
+                { new Item("Crush", 1.99, 2),
+                        new Item("C-Cola", 1.49, 2),
+                        new Item("Berry", 2.49, 1) }
+        };
+
+        Machine machine = new Machine(items);
+
+        System.out.println(machine);
+
+        int choice = 1;
+
+        while (choice == 1) {
+            System.out.print("\nPick a row: ");
+            int row = scan.nextInt();
+            scan.nextLine();
+
+            System.out.print("Pick a spot in the row: ");
+            int spot = scan.nextInt();
+            scan.nextLine();
+
+            if (machine.getItem(row, spot).getQuantity() > 0) {
+                machine.dispense(row, spot);
+                System.out.println("\n" + machine);
+                System.out.print("\nEnoy your drink! Press 1 to purchase another: ");
+                choice = scan.nextInt();
+
+            } else {
+                System.out.print("\nSorry, we're out of this item. Press 1 to purchase another: ");
+                choice = scan.nextInt();
+            }
+        }
+        scan.close();
+    }
+}
